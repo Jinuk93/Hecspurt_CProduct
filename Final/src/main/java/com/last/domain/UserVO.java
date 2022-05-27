26120cb4433814cb31ed6acaae5d16d6efc697b0
Cladonia-S <rlaalswn129@gmail.com>
2022-05-27 17:30:54 +0900

    Create UserVO.java

---
+++ Final/src/main/java/com/last/domain/UserVO.java
@@ -0,0 +1,16 @@
+package com.last.domain;
+
+import lombok.Data;
+
+@Data
+
+public class UserVO {
+	private String userID;
+	private String pwd;
+	private String uName;
+	private String tell;
+	private String email;
+	private String address;
+	private char power;
+
+}
